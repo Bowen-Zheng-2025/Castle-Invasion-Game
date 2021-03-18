8dc3067a54a5a566b0f997035cf5106552d92891
Bowen <bowenzheng2003@gmail.com>
2021-03-18 11:20:23 -0400

    3/17/21 - day's work trying to incorporate barrel

--- environment/map.js
+++ environment/map.js
@@ -1,23 +1,45 @@
-//MAKE SURE TO REVERT BACK THE GAME.HTML TO INCLUDE GOODMOVE.JS
+/*
 var c = document.getElementById("myCanvas"); //loads the instructions/code from below into the canvas on the screen
 var ctx = c.getContext("2d"); //gains access to drawing the shapes and stuff from below
 
-var imgPlat = new Image(); //basically creates the image
-imgPlat.onload = function(){ //uploads the image onto the screen
-  drawPlat(); //uses a function from below
+var imgBar = new Image(); //basically creates the image
+imgBar.onload = function(){ //uploads the image onto the screen
+  drawBar(); //uses a function from below
 }
-imgPlat.src="environment/plat.png"; //source for where the image is coming from
+imgBar.src="environment/bar.png"; //source for where the image is coming from
 
-function drawPlat(){
-  //ctx.rotate(0.08);
+var barrel = {xPos: 20, yPos: 20, rad: 25, xMove: 5, yMove: 5};
+var gravity = 0.2; //Sets the gravity pulling the ball to the ground.
+
+function drawBar (){
   ctx.save(); //saves the present condition/state of the image/game
   ctx.beginPath(); //starts drawing the screen
-  ctx.drawImage(imgPlat, 0, 110, 1100, 90); //gives specifications for drawing it
-  ctx.lineWidth = 5;
-  ctx.strokeStyle = "black";
+  ctx.drawImage(imgBar, barrel.xPos, barrel.yPos, 50, 50); //parameters for drawing the bird
   ctx.fill(); //fills the image/drawing
   ctx.stroke(); //finishes the drawing
   ctx.restore(); //reuses the saved image
 }
 
+function draw() {
+  ctx.clearRect(0, 0, myCanvas.width, c.height); //clears the screen
+  drawBar();
   drawPlat();
+  if (barrel.xPos + barrel.xMove > c.width - barrel.rad || barrel.xPos + barrel.xMove < barrel.rad) { //If the circle's x position exceeds the width of the canvas...
+    barrel.xMove = -barrel.xMove; //The ball's x direction will be flipped, and it will bounce a specific distance (damping).
+  }
+
+  if(barrel.yPos + barrel.yMove > c.height - barrel.rad || barrel.yPos + barrel.yMove < barrel.rad) { //If the circle's y position exceeds the height of the canvas...
+    barrel.yMove = -barrel.yMove * damping; //Its y direction will be flipped, and it's speed will decrease.
+  }
+
+  barrel.yMove += gravity; //Adds the gravity value to the ball's dy value, giving it a artificial force of gravity.
+
+  barrel.xPos += barrel.xMove;
+
+  if (((barrel.yPos + barrel.yMove) + barrel.rad) <= c.height) {
+    barrel.yPos += barrel.yMove;
+  }
+}
+
+setInterval(draw, 10); //like a loop that repeats the draw function to keep drawing the shapes after 10 milliseconds
+*/
--- environment/platform.js
+++ environment/platform.js
@@ -1,8 +1,45 @@
-class Platform {
-  constructor(xPos, yPos, width, height) {
-    this.xPos = xPos;
-    this.yPos = yPos;
-    this.width = width;
-    this.height = height;
-  }
+var c = document.getElementById("myCanvas"); //loads the instructions/code from below into the canvas on the screen
+var ctx = c.getContext("2d"); //gains access to drawing the shapes and stuff from below
+
+var imgPlat = new Image(); //basically creates the image
+imgPlat.onload = function(){ //uploads the image onto the screen
+  drawPlat(); //uses a function from below
 }
+imgPlat.src="environment/plat.png"; //source for where the image is coming from
+
+function drawPlat(){
+  //top platform
+  ctx.transform(1, 0.05, -0.05, 1, 50, -60);
+  ctx.save(); //saves the present condition/state of the image/game
+  ctx.beginPath(); //starts drawing the screen
+  ctx.drawImage(imgPlat, -60, 50, 1150, 90); //gives specifications for drawing it
+  ctx.fill(); //fills the image/drawing
+  ctx.stroke(); //finishes the drawing
+  ctx.restore(); //reuses the saved image
+  //platform below the top
+  ctx.transform(1, 0.05, -0.05, 1, 230, 120);
+  ctx.save(); //saves the present condition/state of the image/game
+  ctx.beginPath(); //starts drawing the screen
+  ctx.drawImage(imgPlat, 120, 230, 1150, 90); //gives specifications for drawing it
+  ctx.fill(); //fills the image/drawing
+  ctx.stroke(); //finishes the drawing
+  ctx.restore(); //reuses the saved image
+  //plaform above the bottom platform
+  ctx.transform(1, 0.05, -0.05, 1, -60, 340);
+  ctx.save(); //saves the present condition/state of the image/game
+  ctx.beginPath(); //starts drawing the screen
+  ctx.drawImage(imgPlat, -60, 340, 1150, 90); //gives specifications for drawing it
+  ctx.fill(); //fills the image/drawing
+  ctx.stroke(); //finishes the drawing
+  ctx.restore(); //reuses the saved image
+  //platform on the bottom of the screen
+  ctx.transform(1, 0.05, -0.05, 1, 520, 10);
+  ctx.save(); //saves the present condition/state of the image/game
+  ctx.beginPath(); //starts drawing the screen
+  ctx.drawImage(imgPlat, 10, 520, 1300, 90); //gives specifications for drawing it
+  ctx.fill(); //fills the image/drawing
+  ctx.stroke(); //finishes the drawing
+  ctx.restore(); //reuses the saved image
+}
+
+drawPlat();
