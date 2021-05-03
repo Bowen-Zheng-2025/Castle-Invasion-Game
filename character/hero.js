44ae102220883bf45b71460287d153746483adea
Lilla <54989842+Lilla0802@users.noreply.github.com>
2021-05-03 11:30:50 -0700

    saving new code

--- character/hero.js
+++ character/hero.js
@@ -1,34 +1,9 @@
 var rect = {xPos: 20, yPos: c.height - 150, width: 50, height: 50, xMove: 10, yMove: 50}; //gives specifications for drawing the rectangle
-var grav = 0.5; //the gravity that pulls the hero down to the ground
-const heroFrameWid = 51.5; //the width of a single frame for the hero spritesheet
-const heroFrameHeight = 60; //the height of a single frame for the hero spritesheet
-let frameIndexHero = 0; //counter for the frames for animation
-var imgStill = new Image(); //basically creates the image
-imgStill.onload = function(){ //uploads the image onto the screen
-  heroStill(); //uses a function from below
-}
-imgStill.src="character/climbStop.png"; //source for where the image is coming from
+var grav = 0.5;
 
-/*heroStill()
-draws a cannon for visual effects and for fun
-*/
-function heroStill(){
-  ctx.save(); //saves the present condition/state of the image/game
-  ctx.beginPath(); //starts drawing the screen
-  ctx.drawImage(imgStill, rect.xPos+3, rect.yPos-17, rect.width*.8, rect.height*1.5); //gives specifications for drawing it
-  ctx.fill(); //fills the image/drawing
-  ctx.stroke(); //finishes the drawing
-  ctx.restore(); //reuses the saved image
-}
-
-/*animateHero()
-this function animates the hero standing there blinking in the game.
-This is the default mode.
-*/
 function animateHero(){
-  if ((right == false) && (left == false) && (jump == false) && (stopClimb == false) && (goClimb == false)) {
-    //if everything else is not in use, sets hero blinking as default
-    heroSprite.src = "character/hero.png"; //calls the animation for the hero standing there and blinking
+  if ((right == false) && (left == false) && (jump == false) && (climb == false)) {
+    heroSprite.src = "character/hero.png";
     ctx.drawImage(
       heroSprite,
       frameIndex * heroFrameWid,
@@ -41,19 +16,15 @@ function animateHero(){
       heroFrameHeight * 1.2,
     );
   }
-  if (frameIndex > 2) { //there are only 3 frames of hero blinking
-    frameIndex = 0; //resets the frame for animation
+  if (frameIndex > 2) {
+    frameIndex = 0;
   }
 }
-
-/*animateHeroClimb()
-this function animates the hero climbing in the game
-*/
 function animateHeroClimb(){
-  if ((climb == true) && (jump == false) && (goClimb == true)) { //makes sure that the hero jumping animation is not in use
-    right = false; //turns off the animation for hero moving right
-    left = false; //turns off the animation for the hero moving left
-    heroSprite.src = "character/heroClimb.png"; //calls the animation that draws that hero climbing
+  if ((climb == true) && (jump == false)) {
+    right = false;
+    left = false;
+    heroSprite.src = "character/heroClimb.png";
     ctx.drawImage(
       heroSprite,
       frameIndex * 60,
@@ -65,18 +36,14 @@ function animateHeroClimb(){
       60,
       80,
     );
-    if (frameIndex > 3) { //there are only 4 frames of hero climbing
-      frameIndex = 0; //resets the frame for the animation
+    if (frameIndex > 3) {
+      frameIndex = 0;
     }
   }
 }
-
-/*animateHeroJump()
-this function animates the hero jumping in the game
-*/
 function animateHeroJump(){
-  if (jump == true) { //if the hero jumping boolean is used
-    heroSprite.src = "character/heroJump.png"; //calls the spritesheet that does the hero jumping animation
+  if (jump == true) {
+    heroSprite.src = "character/heroJump.png";
     ctx.drawImage(
       heroSprite,
       frameIndex,
@@ -90,13 +57,9 @@ function animateHeroJump(){
     );
   }
 }
-
-/*animateHeroLeft()
-this function animates the hero moving to the left in the game
-*/
 function animateHeroLeft(){
-  if ((left == true) && (jump == false)) { //makes sure that the jump animation is not in use
-    heroSprite.src = "character/heroLeft.png"; //calls the hero moving left animation
+  if ((left == true) && (jump == false)) {
+    heroSprite.src = "character/heroLeft.png";
     ctx.drawImage(
       heroSprite,
       frameIndex * 84, //the 84 is the hero's width
@@ -108,18 +71,14 @@ function animateHeroLeft(){
       84 * 1,
       65 * 1,
     );
-    if (frameIndex > 7) { // there are only 8 frames for the animation
-      frameIndex = 0; //resets the frames for the animation
+    if (frameIndex > 7) {
+      frameIndex = 0;
     }
   }
 }
-
-/*animateHeroRight()
-this function animates the hero moving right in the game
-*/
 function animateHeroRight(){
-  if ((right == true) && (jump == false)) { //makes sure the hero jumping up animation is not in use
-    heroSprite.src = "character/heroRight.png"; //uses the hero moving to the right animation
+  if ((right == true) && (jump == false)) {
+    heroSprite.src = "character/heroRight.png";
     ctx.drawImage(
       heroSprite,
       frameIndex * 84,
@@ -131,12 +90,11 @@ function animateHeroRight(){
       84 * 1,
       65 * 1,
     );
-    if (frameIndex > 7) { // this spritesheet only has 8 frames
-      frameIndex = 0; //resets the frames for animation
+    if (frameIndex > 7) {
+      frameIndex = 0;
     }
   }
 }
-
 /*collideHeroBad()
 is a collision checker between the hero/rect and the npc
 */
