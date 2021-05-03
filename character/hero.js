c5290509e6b15ba4c55dd6d00cd0ae2888c378a1
Lilla <54989842+Lilla0802@users.noreply.github.com>
2021-05-03 11:30:50 -0700

    Revert "saving new code"
    This reverts commit 44ae102220883bf45b71460287d153746483adea.

--- character/hero.js
+++ character/hero.js
@@ -1,9 +1,34 @@
 var rect = {xPos: 20, yPos: c.height - 150, width: 50, height: 50, xMove: 10, yMove: 50}; //gives specifications for drawing the rectangle
-var grav = 0.5;
+var grav = 0.5; //the gravity that pulls the hero down to the ground
+const heroFrameWid = 51.5; //the width of a single frame for the hero spritesheet
+const heroFrameHeight = 60; //the height of a single frame for the hero spritesheet
+let frameIndexHero = 0; //counter for the frames for animation
+var imgStill = new Image(); //basically creates the image
+imgStill.onload = function(){ //uploads the image onto the screen
+  heroStill(); //uses a function from below
+}
+imgStill.src="character/climbStop.png"; //source for where the image is coming from
 
+/*heroStill()
+draws a cannon for visual effects and for fun
+*/
+function heroStill(){
+  ctx.save(); //saves the present condition/state of the image/game
+  ctx.beginPath(); //starts drawing the screen
+  ctx.drawImage(imgStill, rect.xPos+3, rect.yPos-17, rect.width*.8, rect.height*1.5); //gives specifications for drawing it
+  ctx.fill(); //fills the image/drawing
+  ctx.stroke(); //finishes the drawing
+  ctx.restore(); //reuses the saved image
+}
+
+/*animateHero()
+this function animates the hero standing there blinking in the game.
+This is the default mode.
+*/
 function animateHero(){
-  if ((right == false) && (left == false) && (jump == false) && (climb == false)) {
-    heroSprite.src = "character/hero.png";
+  if ((right == false) && (left == false) && (jump == false) && (stopClimb == false) && (goClimb == false)) {
+    //if everything else is not in use, sets hero blinking as default
+    heroSprite.src = "character/hero.png"; //calls the animation for the hero standing there and blinking
     ctx.drawImage(
       heroSprite,
       frameIndex * heroFrameWid,
@@ -16,15 +41,19 @@ function animateHero(){
       heroFrameHeight * 1.2,
     );
   }
-  if (frameIndex > 2) {
-    frameIndex = 0;
+  if (frameIndex > 2) { //there are only 3 frames of hero blinking
+    frameIndex = 0; //resets the frame for animation
   }
 }
+
+/*animateHeroClimb()
+this function animates the hero climbing in the game
+*/
 function animateHeroClimb(){
-  if ((climb == true) && (jump == false)) {
-    right = false;
-    left = false;
-    heroSprite.src = "character/heroClimb.png";
+  if ((climb == true) && (jump == false) && (goClimb == true)) { //makes sure that the hero jumping animation is not in use
+    right = false; //turns off the animation for hero moving right
+    left = false; //turns off the animation for the hero moving left
+    heroSprite.src = "character/heroClimb.png"; //calls the animation that draws that hero climbing
     ctx.drawImage(
       heroSprite,
       frameIndex * 60,
@@ -36,14 +65,18 @@ function animateHeroClimb(){
       60,
       80,
     );
-    if (frameIndex > 3) {
-      frameIndex = 0;
+    if (frameIndex > 3) { //there are only 4 frames of hero climbing
+      frameIndex = 0; //resets the frame for the animation
     }
   }
 }
+
+/*animateHeroJump()
+this function animates the hero jumping in the game
+*/
 function animateHeroJump(){
-  if (jump == true) {
-    heroSprite.src = "character/heroJump.png";
+  if (jump == true) { //if the hero jumping boolean is used
+    heroSprite.src = "character/heroJump.png"; //calls the spritesheet that does the hero jumping animation
     ctx.drawImage(
       heroSprite,
       frameIndex,
@@ -57,9 +90,13 @@ function animateHeroJump(){
     );
   }
 }
+
+/*animateHeroLeft()
+this function animates the hero moving to the left in the game
+*/
 function animateHeroLeft(){
-  if ((left == true) && (jump == false)) {
-    heroSprite.src = "character/heroLeft.png";
+  if ((left == true) && (jump == false)) { //makes sure that the jump animation is not in use
+    heroSprite.src = "character/heroLeft.png"; //calls the hero moving left animation
     ctx.drawImage(
       heroSprite,
       frameIndex * 84, //the 84 is the hero's width
@@ -71,14 +108,18 @@ function animateHeroLeft(){
       84 * 1,
       65 * 1,
     );
-    if (frameIndex > 7) {
-      frameIndex = 0;
+    if (frameIndex > 7) { // there are only 8 frames for the animation
+      frameIndex = 0; //resets the frames for the animation
     }
   }
 }
+
+/*animateHeroRight()
+this function animates the hero moving right in the game
+*/
 function animateHeroRight(){
-  if ((right == true) && (jump == false)) {
-    heroSprite.src = "character/heroRight.png";
+  if ((right == true) && (jump == false)) { //makes sure the hero jumping up animation is not in use
+    heroSprite.src = "character/heroRight.png"; //uses the hero moving to the right animation
     ctx.drawImage(
       heroSprite,
       frameIndex * 84,
@@ -90,11 +131,12 @@ function animateHeroRight(){
       84 * 1,
       65 * 1,
     );
-    if (frameIndex > 7) {
-      frameIndex = 0;
+    if (frameIndex > 7) { // this spritesheet only has 8 frames
+      frameIndex = 0; //resets the frames for animation
     }
   }
 }
+
 /*collideHeroBad()
 is a collision checker between the hero/rect and the npc
 */
--- functionality/goodMove.js
+++ functionality/goodMove.js
@@ -1,9 +1,11 @@
 var c = document.getElementById("myCanvas"); //loads the instructions/code from below into the canvas on the screen
 var ctx = c.getContext("2d"); //gains access to drawing the shapes and stuff from below
-var right = false;
-var left = false;
-var jump = false;
-var climb = false;
+var right = false; //boolean tracker to check if hero "moving right" animation should be implemented
+var left = false; //boolean tracker to check if hero "moving left" animation should be implemented
+var jump = false; //boolean tracker to check if hero "jumping up" animation should be implemented
+var climb = false; //boolean tracker to check if hero "climbing up" animation should be implemented
+var goClimb = false; //a second boolean tracker in addition to 'climb' to check if hero "climbing up" animation should be implemented
+var stopClimb = false; //boolean tracker to check if hero "climbing up" still picture should be implemented (for animation effects)
 
 /*collideHeroPlat()
 checks for collisions between the hero and the platforms
@@ -66,50 +68,97 @@ function collideHeroBar(){
   }
 }
 
+/*jumpCheck()
+this function checks to see if the hero should be jumping or not
+*/
 function jumpCheck(){
   //this if is if the hero is in between the third and fourth platform
   if ((rect.yPos>plat.yPos+plat.height+305)&&(rect.yPos+rect.height<plat.yPos+465)) {
-    jump = true;
+    jump = true; //let the hero jump
   }
   //this if is if the hero is in between the second and third platform
   else if ((rect.yPos>plat.yPos+plat.height+155)&&(rect.yPos<plat.yPos+305)&&(rect.yPos+rect.height<plat.yPos+300)) {
-    jump = true;
+    jump = true; //let the hero jump
   }
   //this if is if the hero is in between the first and second platform
   else if ((rect.yPos>plat.yPos+plat.height)&&(rect.yPos<plat.yPos+155)&&(rect.yPos+rect.height<plat.yPos+150)) {
-    jump = true;
+    jump = true; //let the hero jump
   }
   //this if is if the hero is above the first platform
   else if ((rect.yPos<plat.yPos)&&(rect.yPos+rect.height<plat.yPos-5)) {
-    jump = true;
+    jump = true; //let the hero jump
   }
+  //this else if fixes a bug so that the hero can look like it's jumping when it's goes off the edge of the first platform
+  else if ((rect.xPos>plat.width-10)&&(rect.yPos>0)&&(rect.yPos+rect.height<plat.yPos+150)) {
+    jump = true; //let the hero jump
+  }
+  //this else if fixes a bug so that the hero can look like it's jumping when it's goes off the edge of the second platform
+  else if ((rect.xPos<200)&&(rect.yPos>plat.yPos+plat.height)&&(rect.yPos+rect.height<plat.yPos+300)) {
+    jump = true; //let the hero jump
+  }
+  //this else if fixes a bug so that the hero can look like it's jumping when it's goes off the edge of the third platform
+  else if ((rect.xPos>plat.width-10)&&(rect.yPos>plat.yPos+plat.height+155)&&(rect.yPos+rect.height<plat.yPos+465)) {
+    jump = true; //let the hero jump
+  }
+  //anything thing else that I didn't mention above that doesn't qualify the hero to jump, don't let it jump
   else {
-    jump = false;
+    jump = false; //let the hero not jump
   }
 }
 
+/*heroClimb()
+this functions checks to see if the hero should climb. If it does, stop the jumping animation
+*/
 function heroClimb(){
-  for (var i = 0; i < ladArr.length; i++) {
+  for (var i = 0; i < ladArr.length; i++) { //goes through the ladder array
     if (rect.yPos>plat.yPos+plat.height+305) { //if the hero is below the third platform
+      //if the hero is within the ladder's vicinity
       if ((ladArr[i].yPos==plat.yPos+305)&&(rect.xPos>ladArr[i].xPos-20)&&(rect.xPos+rect.width<ladArr[i].xPos+ladArr[i].width+20)) {
-        climb = true;
-        jump = false;
+        climb = true; //use the hero climbing animation
+        jump = false; //stop the hero jumping animation
+      }
+      //This manually turns off climbing animation if the hero is not in the ladder's vicinity
+      if (rect.xPos < ladArr[i].xPos - 20) {
+        climb = false; //stop the hero climbing animation
+      }
+      //This manually turns off climbing animation if the hero is not in the ladder's vicinity
+      if (rect.xPos + rect.width > ladArr[i].xPos + ladArr[i].width + 20) {
+        climb = false; //stop the hero climbing animation
       }
     }
-    else if ((rect.yPos<plat.yPos+305)&&(rect.yPos>plat.yPos+plat.height+155)) {
+    else if ((rect.yPos<plat.yPos+305)&&(rect.yPos>plat.yPos+plat.height+155)) { //if the hero is between the second and third platform
+      //if the hero is within the ladder's vicinity
       if ((ladArr[i].yPos==plat.yPos+155)&&(rect.xPos>ladArr[i].xPos-20)&&(rect.xPos+rect.width<ladArr[i].xPos+ladArr[i].width+20)) {
-        climb = true;
-        jump = false;
+        climb = true; //use the hero climbing animation
+        jump = false; //stop the hero jumping animation
+      }
+      //This manually turns off climbing animation if the hero is not in the ladder's vicinity
+      if ((rect.xPos < ladArr[i].xPos - 20) && (ladArr[i].yPos==plat.yPos+155)) {
+        climb = false; //stop the hero climbing animation
+      }
+      //This manually turns off climbing animation if the hero is not in the ladder's vicinity
+      if ((rect.xPos+rect.width>ladArr[i].xPos+ladArr[i].width+20) && (ladArr[i].yPos==plat.yPos+155)) {
+        climb = false; //stop the hero climbing animation
       }
     }
-    else if ((rect.yPos<plat.yPos+155)&&(rect.yPos>plat.yPos+plat.height)) {
+    else if ((rect.yPos<plat.yPos+155)&&(rect.yPos>plat.yPos+plat.height)) { //if the hero is between the first and second platform
+      //if the hero is within the ladder's vicinity
       if ((ladArr[i].yPos==plat.yPos)&&(rect.xPos>ladArr[i].xPos-20)&&(rect.xPos+rect.width<ladArr[i].xPos+ladArr[i].width+20)) {
-        climb = true;
-        jump = false;
+        climb = true; //use the hero climbing animation
+        jump = false; //stop the hero jumping animation
+      }
+      //This manually turns off climbing animation if the hero is not in the ladder's vicinity
+      if ((rect.xPos < ladArr[i].xPos - 20) && (ladArr[i].yPos==plat.yPos)) {
+        climb = false; //stop the hero climbing animation
+      }
+      //This manually turns off climbing animation if the hero is not in the ladder's vicinity
+      if ((rect.xPos+rect.width>ladArr[i].xPos+ladArr[i].width+20) && (ladArr[i].yPos==plat.yPos)) {
+        climb = false; //stop the hero climbing animation
       }
     }
+    //anything other condition I didn't specify, turn off the climbing animation
     else {
-      climb = false;
+      climb = false; //stop the hero climbing animation
     }
   }
 }
@@ -117,24 +166,28 @@ function heroClimb(){
 document.addEventListener("keydown", makeMove); //addEventListenerws users to hit a key on keyboard to interact with the objects
 function makeMove(e) {
   if ((e.key == "d") && (rect.xPos + rect.width < c.width)) { //lets hero move to the right
-    right = true;
+    right = true; //enables hero moving right animation
     rect.xPos += rect.xMove;
   }
   if ((e.key == "a") && (rect.xPos > 0)) { //lets hero move to the left
-    left = true;
+    left = true; //enables hero moving left animation
     rect.xPos -= rect.xMove;
   }
   //the problem is that I can only use one of these ifs at a time
   if (e.key == "w") { //lets the hero move up (basically jump)
-    for (var i = 0; i < ladArr.length; i++){
+    for (var i = 0; i < ladArr.length; i++){ //goes through the ladder array
       //this if checks if the hero is within the vicinity of ladder (which is between the third and fourth platform)
       if (rect.yPos>plat.yPos+305) {
         if ((ladArr[i].yPos==plat.yPos+305)&&(rect.xPos>ladArr[i].xPos-20)&&(rect.xPos+rect.width<ladArr[i].xPos+ladArr[i].width+20)) {
           rect.yMove = 5; //makes it climb slower so it looks cooler and for cool effects
           noGrav = true; //turns off gravity
+          goClimb = true; //turns on the climbing animation
+          stopClimb = true; //turns on the still image of hero
           if ((rect.yPos <= plat.yPos+305+plat.height)) { //checks to make sure that the hero moves past and lands on the third platform
             noGrav = false; //turns gravity back on
-            rect.yMove = 50;
+            goClimb = false; //turns on the climbing animation
+            stopClimb = false; //turns on the still image of hero
+            rect.yMove = 50; //reset the hero's distance it can move in the y direction
             rect.yPos = plat.yPos + 305 - rect.height; //lands on the next level (which is the third platform)
           }
         }
@@ -144,9 +197,13 @@ function makeMove(e) {
         if ((ladArr[i].yPos==plat.yPos+155)&&(rect.xPos>ladArr[i].xPos-20)&&(rect.xPos+rect.width<ladArr[i].xPos+ladArr[i].width+20)) {
           rect.yMove = 5; //makes it climb slower so it looks cooler and for cool effects
           noGrav = true; //turns off gravity
+          goClimb = true; //turns on the climbing animation
+          stopClimb = true; //turns on the still image of hero
           if ((rect.yPos <= plat.yPos+155+plat.height)) { //checks to make sure that the hero moves past and lands on the third platform
             noGrav = false; //turns gravity back on
-            rect.yMove = 50;
+            goClimb = false; //turns on the climbing animation
+            stopClimb = false; //turns on the still image of hero
+            rect.yMove = 50; //reset the hero's distance it can move in the y direction
             rect.yPos = plat.yPos + 155 - rect.height; //jumps to the next level (which is the second platform)
           }
         }
@@ -156,9 +213,13 @@ function makeMove(e) {
         if ((ladArr[i].yPos==plat.yPos)&&(rect.xPos>ladArr[i].xPos-20)&&(rect.xPos+rect.width<ladArr[i].xPos+ladArr[i].width+20)) {
           rect.yMove = 5; //makes it climb slower so it looks cooler and for cool effects
           noGrav = true; //turns off gravity
+          goClimb = true; //turns on the climbing animation
+          stopClimb = true; //turns on the still image of hero
           if ((rect.yPos <= plat.yPos+plat.height)) { //checks to make sure that the hero moves past and lands on the third platform
             noGrav = false; //turns gravity back on
-            rect.yMove = 50;
+            goClimb = false; //turns on the climbing animation
+            stopClimb = false; //turns on the still image of hero
+            rect.yMove = 50; //reset the hero's distance it can move in the y direction
             rect.yPos = plat.yPos - rect.height; //jumps to the next level (which is the first platform)
           }
         }
@@ -166,46 +227,37 @@ function makeMove(e) {
     }
     rect.yPos -= rect.yMove; //allows the hero to jump up
   }
-
-  // for (var i = 0; i < ladArr.length; i++){
-  //   //this if checks if the hero is within the vicinity of ladder (which is between the third and fourth platform)
-  //   if (rect.yPos>plat.yPos+305) {
-  //     if ((ladArr[i].yPos==plat.yPos+305)&&(rect.xPos>ladArr[i].xPos-20)&&(rect.xPos+rect.width<ladArr[i].xPos+ladArr[i].width+20)) {
-  //       rect.yMove = 5; //makes it climb slower so it looks cooler and for cool effects
-  //       noGrav = true; //turns off gravity
-  //       if ((rect.yPos <= plat.yPos+305+plat.height)) { //checks to make sure that the hero moves past and lands on the third platform
-  //         noGrav = false; //turns gravity back on
-  //         rect.yMove = 50;
-  //         rect.yPos = plat.yPos + 305 - rect.height; //lands on the next level (which is the third platform)
-  //       }
-  //     }
-  //   }
-
   if (e.key == "s") { //lets the hero climb down ladders
     for (var i = 0; i < ladArr.length; i++) { //goes through the ladder array
       //this if checks if the hero is within the vicinity of ladder (which is between the third and fourth platform)
       if (rect.yPos+rect.height>plat.yPos+300) { //if the rect is below the third platform, follow these steps
-        if ((rect.yPos+rect.height+5>plat.yPos+305)&&(rect.yPos<plat.yPos+plat.height+305)) {
+        if ((rect.yPos+rect.height+5>plat.yPos+305)&&(rect.yPos<plat.yPos+plat.height+305)&&(ladArr[i].yPos==plat.yPos+305)&&(rect.xPos>ladArr[i].xPos-20)&&(rect.xPos+rect.width<ladArr[i].xPos+ladArr[i].width+20)) {
           rect.yPos = plat.yPos+plat.height+305; //gives illusion that rect is moving down to the next ladder rung
         }
         rect.yMove = 5; //changes speed of hero climbing for cool effect
         noGrav = true; //turns off gravity
+        goClimb = true; //turns on the climbing animation
+        stopClimb = true; //turns on the still image of hero
       }
       //this if checks if the hero is within the vicinity of ladder (which is between the second and third platform)
       if (rect.yPos+rect.height>plat.yPos+150) { //if the rect is below the second platform, follow these steps
-        if ((rect.yPos+rect.height+5>plat.yPos+155)&&(rect.yPos<plat.yPos+plat.height+155)) {
+        if ((rect.yPos+rect.height+5>plat.yPos+155)&&(rect.yPos<plat.yPos+plat.height+155)&&(ladArr[i].yPos==plat.yPos+155)&&(rect.xPos>ladArr[i].xPos-20)&&(rect.xPos+rect.width<ladArr[i].xPos+ladArr[i].width+20)) {
           rect.yPos = plat.yPos+plat.height+155; //gives illusion that rect is moving down to the next ladder rung
         }
         rect.yMove = 5; //changes speed of hero climbing for cool effect
         noGrav = true; //turns off gravity
+        goClimb = true; //turns on the climbing animation
+        stopClimb = true; //turns on the still image of hero
       }
       //this if checks if the hero is within the vicinity of ladder (which is between the first and second platform)
       if (rect.yPos+rect.height>plat.yPos-5) { //if the rect is below the second platform, follow these steps
-        if ((rect.yPos+rect.height+5>plat.yPos)&&(rect.yPos<plat.yPos+plat.height)) {
+        if ((rect.yPos+rect.height+5>plat.yPos)&&(rect.yPos<plat.yPos+plat.height)&&(ladArr[i].yPos==plat.yPos)&&(rect.xPos>ladArr[i].xPos-20)&&(rect.xPos+rect.width<ladArr[i].xPos+ladArr[i].width+20)) {
           rect.yPos = plat.yPos+plat.height; //gives illusion that rect is moving down to the next ladder rung
         }
         rect.yMove = 5; //changes speed of hero climbing for cool effect
         noGrav = true; //turns off gravity
+        goClimb = true; //turns on the climbing animation
+        stopClimb = true; //turns on the still image of hero
       }
     } //end of the for loop for the 's' key
     rect.yPos += rect.yMove; //makes the hero move down ladder
@@ -214,10 +266,16 @@ function makeMove(e) {
 
 document.addEventListener("keyup", pauser); //addEventListenerws users to hit a key on keyboard to interact with the objects
 function pauser(e){
-  if (e.key == "d") {
-    right = false;
+  if (e.key == "d") { //if the player stop pressing the 'd' key, turn off the hero moving right animation
+    right = false; //turns off the hero moving right animation
   }
-  if (e.key == "a") {
-    left = false;
+  if (e.key == "a") { //if the player stop pressing the 'a' key, turn off the hero moving left animation
+    left = false; //turns off the hero moving left animation
+  }
+  if (e.key == "w") { //if the player stop pressing the 'a' key, turn off the hero moving left animation
+    goClimb = false; //turns off the hero moving left animation
+  }
+  if (e.key == "s") { //if the player stop pressing the 'a' key, turn off the hero moving left animation
+    goClimb = false; //turns off the hero moving left animation
   }
 }
